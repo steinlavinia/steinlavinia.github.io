6a2727d04f52f243be704fa9de127db3dc5f07a6
steinlavinia <stein.lavinia@estudante.ifro.edu.br>
2024-10-30 08:25:32 -0400

    commit

--- Front-End II/Exercicios/aula.04/script.js
+++ Front-End II/Exercicios/aula.04/script.js
@@ -1,4 +1,35 @@
+const p = document.querySelector('#paragrafo');
+const divImg = document.querySelector('#divImg');
+
+// divImg.addEventListener('click', () => {
+//     divImg.style.border = '2px solid purple';
+// })
+
+function pMouse() {
+    divImg.style.border = '2px solid purple';
+}
+
+function sMouse () {
+    divImg.style.border = 'none';
+}
+
+divImg.addEventListener('mouseover', pMouse);
+divImg.addEventListener('mouseout', sMouse);
+
+p.onclick = () => {
+    alert('Parágrafo Clicado!');
+}
+
 const clique = () => {
-    console.log ('Botão Clicado!')
-    alert('Botão Clicado!')
+    console.log ('Botão Clicado!');
+    alert('Botão Clicado!');
+}
+
+function pressTecla () {
+    if (window.getComputedStyle(divImg, null).getPropertyValue
+    ('background-color') === 'rgb(0, 0, 0)') {
+        divImg.style.backgroundColor = 'rgb(255, 255, 255)'
+    } else {
+        divImg.style.backgroundColor = 'rgb(0, 0, 0)'
+    } 
 }
