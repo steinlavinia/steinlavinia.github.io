d91b3b248038305f6889f6011979edd63c009641
steinlavinia <stein.lavinia@estudante.ifro.edu.br>
2024-11-08 08:22:35 -0400

    commit

--- projeto1/src/components/header/index.js
+++ projeto1/src/components/header/index.js
@@ -5,6 +5,7 @@ export default function Header() {
         <header>
             <nav>
                 <ul>
+                    <li><Link href="/">Home</Link></li>
                     <li><Link href="/sobre">Sobre</Link></li>
                     <li><Link href="/contato">Contato</Link></li>
                 </ul>
